a467559ca7b2df83990e3099e667b90b2f387460
Gaurigoyanka <73536289+Gaurigoyanka@users.noreply.github.com>
2020-11-20 14:01:04 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,25 @@
+class Ball{
+    constructor(x,y,r){
+        var options={
+           restitution:0.8,
+           friction:0.3,
+           density:1.0
+        }
+    this.body=Bodies.circle(x,y,r,options)
+    this.r=r
+   
+    World.add(world,this.body)
+    }
+    display(){
+    var pos=this.body.position
+    var angle=this.body.angle
+    push()
+    //translate(pos.x,pos.y)
+    //rotate(angle)
+    rectMode(CENTER)
+    strokeWeight(4)
+    fill("white")
+    circle(pos.x,pos.y,this.r)
+    pop()
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+     
+	 box1=new Box(600,580,200,15);
+	 box2=new Box(490,560,15,70);
+	 box3=new Box(690,560,15,70);
+	 ball1=new Ball(690,300,30);
+	 ground=Bodies.rectangle(400,600,900,20,{isStatic:true})
+	 Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  rect(ground.position.x,ground.position.y,900,20);
+  box1.display();
+  box2.display();
+  box3.display();
+  ball1.display();
+  drawSprites();
+ 
+}
+
+
+
